684caa97a7e97d265a4285e8965a25ea3a170307
davidsfc16 <davidsfc16@gmail.com>
2026-04-20 18:59:44 +0200

    cambios 6

--- src/App.js
+++ src/App.js
@@ -183,7 +183,7 @@ const getWeeksWithShifts = useCallback(() => {
   });
 
   return Array.from(weekMap.values()).sort((a, b) => b.weekStart - a.weekStart);
-}, [employees]);
+}, [employees, getStartOfWeek, getEndOfWeek]);
 
 const hacerBackup = async (employees) => {
   try {
